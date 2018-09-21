b6288b4372ddc706ca8b7a295be90d44c6c1bd64
Thomas <broadbentt19@wells-cathedral-school.com>
2018-09-21 11:30:34 +0100

    Figuring out how to use Numpy
    Just a bit of playing around, going to come back to this tonight.
    Numpy seems to be VERY power at this sort of thing so that's where I'm going so far.
    The algorithm itself won't be too hard.

--- PrimAlgorithmDistanceMatrix.py
+++ PrimAlgorithmDistanceMatrix.py
@@ -1,2 +1,26 @@
 import numpy as np
 
+dMatrix = np.array([[None, 27, 12, 23, 74],
+           [27, None, 47, 15, 71],
+           [12, 47, None, 28, 84],
+           [23, 15, 28, None, 15],
+           [24, 71, 87, 15, None]])
+
+
+# Choose any Vertex
+Columnns = np.array([0])
+
+#Delete the Row from Vertex
+dMatrix = np.delete(dMatrix, Columns[-1], 0)
+
+# Get the smallest from each
+smallestInColumn = dMatrix.argmin(axis=1)[Columns]
+
+smallestOverallIndex = dMatrix[smallestInColumn, Columns]
+
+
+print(dMatrix)
+print(dMatrix[0,:])
+
+
+
