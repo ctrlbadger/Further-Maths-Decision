80e97541d3c7503c9b9c9f7accb894e29bd0c12e
Thomas <broadbentt19@wells-cathedral-school.com>
2018-09-24 22:42:03 +0100

    Finished Prims Algorithm
    Okay, 
    implementation is really sloppy. 
    What should happen is that rows should be deleted before sorting but I couldn't figure out how to get that to work with just my Distance Matrix.
    I think I would have to store some sort of meta data about value which I think would feel silly.
    In total, this was a learning excercise for me on how deal with arrays in numpy. 
    Also, I know about Prim's Algorithm far too much now. This code should have taken an afternoon

--- PrimAlgorithmDistanceMatrix.py
+++ PrimAlgorithmDistanceMatrix.py
@@ -1,26 +1,34 @@
 import numpy as np
 
-dMatrix = np.array([[None, 27, 12, 23, 74],
-           [27, None, 47, 15, 71],
-           [12, 47, None, 28, 84],
-           [23, 15, 28, None, 15],
-           [24, 71, 87, 15, None]])
-
-
-# Choose any Vertex
-Columnns = np.array([0])
-
-#Delete the Row from Vertex
-dMatrix = np.delete(dMatrix, Columns[-1], 0)
-
-# Get the smallest from each
-smallestInColumn = dMatrix.argmin(axis=1)[Columns]
-
-smallestOverallIndex = dMatrix[smallestInColumn, Columns]
-
-
-print(dMatrix)
-print(dMatrix[0,:])
+dMatrix = np.array([[100, 27, 12, 23, 74],
+                    [27, 100, 47, 15, 71],
+                    [12, 47, 100, 28, 84],
+                    [23, 15, 28, 100, 75],
+                    [74, 71, 84, 75, 100]])
 
 
 
+
+chosenColumn = [0]
+
+spanningTree = []
+while len(spanningTree) < dMatrix.shape[0] - 1:
+    arrays = np.argsort(dMatrix[:, chosenColumn], axis=None)
+    newArray = []
+
+
+    list(map(lambda x: newArray.append(x) if all([np.floor(x / len(chosenColumn)) != columnIndex for columnIndex in chosenColumn]) else None, arrays))
+    flatIndex = newArray[0]
+    column = chosenColumn[flatIndex % len(chosenColumn)]
+    row = int(np.floor(flatIndex / len(chosenColumn)))
+    spanningTree.append([row, column])
+
+    chosenColumn.append(row)
+
+    print("chosenColumn", chosenColumn)
+
+    print("column", column)
+    print("row", row)
+    print()
+
+print(spanningTree)
